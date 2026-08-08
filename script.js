df269ea6174a869411f58573c9eba413aa7b8e33
Medikurthi Lakshmi Narasimha <lakshminarasimha96m@gmail.com>
2026-08-09 00:07:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,10 +43,8 @@ const favoritePhotoPaths = [
   'images/fav_photos/IMG20260403151945.jpg',
   'images/fav_photos/IMG20260610171743.jpg',
   'images/fav_photos/IMG20260610185513.jpg',
-  'images/fav_photos/Snapchat-712138475.jpg',
   'images/fav_photos/IMG20260406091717.jpg',
-  'images/fav_photos/IMG-20260719-WA0005.jpg',
-  'images/fav_photos/IMG20260610125212.jpg'
+  'images/fav_photos/IMG-20260719-WA0005.jpg'
 ];
 const letterText = document.getElementById('letterText');
 const startJourney = document.getElementById('startJourney');
